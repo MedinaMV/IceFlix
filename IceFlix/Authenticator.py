55b5d3053376850c8f2a90da804cab6d8bfd0895
MedinaMV <valderasalejandro53@gmail.com>
2023-01-12 15:48:27 -0500

    Implementado anunciamientos

--- IceFlix/Authenticator.py
+++ IceFlix/Authenticator.py
@@ -24,8 +24,9 @@ class Authenticator(IceFlix.Authenticator):
     def __init__(self,adminToken):
         self.id = str(uuid.uuid4())
         self.adminToken = adminToken
-        self.proxies = []
+        self.proxies = {}
         self.userUpdate = None
+        self.announcement = None
         try:
             with open(PATH_USERS,'r') as fd:
                 self.users = json.load(fd)
@@ -101,15 +102,15 @@ class Authenticator(IceFlix.Authenticator):
         self.userUpdate.newUser(user,passwordHash,self.id)
 
     def removeUser(self,user,adminToken,current=None):
-        # Si el token suministrado no es el del admin, se lanza la excepcion.
+        """Si el token suministrado no es el del admin, se lanza la excepcion."""
         if not self.isAdmin(adminToken):
             raise IceFlix.Unauthorized
         
-        # Si el usuario no existe, no se puede borrar.
+        """Si el usuario no existe, no se puede borrar."""
         if not self.users.get(user):
             raise IceFlix.Unauthorized
 
-        # Borramos al usuario de nuestra estructura de datos y del archivo json.
+        """Borramos al usuario de nuestra estructura de datos y del archivo json."""
         self.users.pop(user)
         with open(PATH_USERS,'w') as fd:
             json.dump(self.users,fd)
@@ -167,8 +168,12 @@ class Announcement(IceFlix.Announcement):
     def __init__(self,auth:Authenticator):
         self.auth = auth
 
-    def announce(self,service,serviceId):
-        print("Hello World")
+    def announce(self,service,serviceId,current=None):
+        if(serviceId not in self.auth.proxies):
+            self.auth.proxies[serviceId] = service
+            print("ServiceId:  ",serviceId, " ",service , "stored")
+        else:
+            print("ServiceId:  ",serviceId, " ",service , "ignored")
 
 class Server(Ice.Application):
     def run(self, argv):
@@ -182,45 +187,76 @@ class Server(Ice.Application):
 
         adapter.activate()
 
-        """try:
-            # Iniciamos el hilo que anuncia el servicio cada 30s.
-            hilo1 = threading.Thread(target=self.anunciarServicio,args=(prxMain,servant.id,prx,))
-            hilo1.start()
+        servant_discovery = Announcement(servant)
+        proxy_discovery = adapter.addWithUUID(servant_discovery)
+        topic_ann = topics.getTopic(topics.getTopicManager(broker),'ServiceAnnouncements')
+        topic_ann.subscribeAndGetPublisher({},proxy_discovery)
+        publisher_ann = topics.getTopic(topics.getTopicManager(broker),'ServiceAnnouncements').getPublisher()
+        servant.announcement = IceFlix.AnnouncementPrx.uncheckedCast(publisher_ann)
 
-            # Iniciamos el hilo (que se ejecuta cada 30s) que revoca los tokens con un tiempo de vida de 2mins o superior.
-            hilo2 = threading.Thread(target=self.revocarToken,args=(servant,))
-            hilo2.start()
-        except Ice.ConnectionRefusedException:
-            print("El servicio Main no se encuentra disponible...")
+        t = threading.Timer(12,self.startUpService,[servant])
+        t.start()
+
+        if(len(servant.proxies) == 0):
             self.communicator().shutdown()
+
+        try:
+            h1 = threading.Thread(target=self.anunciarServicio,args=(servant,prx,))
+            h1.start()
+            h2 = threading.Thread(target=self.revocarToken,args=(servant,))
+            h2.start()
         except KeyboardInterrupt:
-            hilo1.kill()
-            hilo2.kill()
-            self.communicator().shutdown()"""
+            h1.kill()
+            h2.kill()
+            self.communicator().shutdown()
 
         servant_updates = UserUpdate(servant)
         proxy_updates = adapter.addWithUUID(servant_updates)
         topic_updates = topics.getTopic(topics.getTopicManager(broker),'UserUpdates')
-        topic_updates.subscribeAndGetPublisher({}, proxy_updates)
-        publisher_updates = topics.getTopic(topics.getTopicManager(broker), 'UserUpdates').getPublisher()
+        topic_updates.subscribeAndGetPublisher({},proxy_updates)
+        publisher_updates = topics.getTopic(topics.getTopicManager(broker),'UserUpdates').getPublisher()
         servant.userUpdate = IceFlix.UserUpdatePrx.uncheckedCast(publisher_updates)
 
         self.shutdownOnInterrupt()
         broker.waitForShutdown()
+        topic_ann.unsubscribe(proxy_discovery)
+        topic_updates.unsubscribe(proxy_updates)
 
         return 0
         
-    def anunciarServicio(self,prxmain,servantID,authprx):
-        while True:
-            # Esperamos 30s para realizar un announce.
-            time.sleep(30)
-            try:
-                prxmain.announce(authprx,servantID)
-            except Ice.ConnectionRefusedException:
-                print("El servicio main no se encuentra disponible... ")
-                break
+    def startUpService(self,auth:Authenticator):
+        if(len(auth.proxies) == 0):
+            return
+            
+        authenticator = None
+        main = None
 
-    def revocarToken(self,auth:Authenticator):
+        for value in auth.proxies.values():
+            if(IceFlix.AuthenticatorPrx.checkedCast(value)):
+                authenticator = value
+            elif(IceFlix.MainPrx.checkedCast(value)):
+                main = value
+
+        if main == None:
+            auth.proxies = {}
+            return
+
+        if authenticator != None:
+            authData = authenticator.bulkUpdate()
+            auth.adminToken = authData.adminToken
+            for i in authData.currentUsers:
+                try:
+                    token = authData.activeTokens.get(i)
+                except KeyError:
+                    token = secrets.token_hex(16)
+
+                auth.users[i] = [{"token":token,"passwordHash":authData.currentUsers.get(i),
+                "timestamp":time.mktime(datetime.datetime.now().timetuple())}]
+
+    def anunciarServicio(self,servant,prx):
+        servant.announcement.announce(prx,servant.id)
+
+    def revokeToken(self,auth:Authenticator):
         while True:
             for i in auth.users:
                 if (time.mktime(datetime.datetime.now().timetuple()) - auth.users.get(i)[0]["timestamp"]) >= 120:
